073b9c142f645f8441630f3e1a7fc017637bb96c
AldoLeonard <125373501+AldoLeonard@users.noreply.github.com>
2023-03-11 20:56:00 +0700

    Add files via upload

---
+++ 4b.php
@@ -0,0 +1,30 @@
+<?php
+$hardware = ['Motherboard', 'Processor', 'Hard Disk', 'PC Coller', 'VGA Card', 'SSD'];
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>4b</title>
+</head>
+<body>
+    <h2>Macam-macam perangkat keras komputer</h2>
+    <ol>
+    <?php foreach ($hardware as $h) { ?>
+        <li><?= $h; ?></li>
+        <?php } ?>
+    </ol>
+    <h2>Macam-macam perangkat keras komputer baru</h2>
+    <ol>
+    <?php array_push($hardware, 'Card Raeder', 'Modem') ?>
+    <?php sort($hardware); ?>
+    <?php foreach ($hardware as $h) { ?>
+        <li><?= $h; ?></li>
+        <?php } ?>
+    </ol>
+</body>
+</html>
